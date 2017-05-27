28125e84682843627ee063cc5fe47e5c44c0ee04
Vinnie Chen <vinnie.chen211@gmail.com>
2017-05-26 22:46:57 -0700

    pet file

--- PetWorld/Pet.swift
+++ PetWorld/Pet.swift
@@ -9,6 +9,48 @@
 import UIKit
 import Parse
 
-class Pet {
+class Pet: NSObject {
+
+    var image: UIImage?
+    var name: String?
+    var breed: String?
+    var age: Int?
+    var gender: String?
+    var followers: Int?
+    var following: Int?
     
+    init(name: String, image: UIImage, breed: String, age: Int, gender: String ) {
+        self.name = name
+        self.image = image
+        self.breed = breed
+        self.age = age
+        self.gender = gender
+    }
+    
+    class func addPet(photo: UIImage, name: String, image: UIImage, breed: String, age: Int, gender: String, success: PFBooleanResultBlock?) {
+        let pet = PFObject(className: "Pet")
+        pet["photo"] = getPhotoFile(photo: photo)
+        pet["owner"] = PFUser.current() // Pointer column type that points to PFUser
+        
+        pet["name"] = name
+        pet["breed"] = breed
+        pet["age"] = age
+        pet["followersCount"] = 0
+        pet["followingCount"] = 0
+        
+        // Save object (following function will save the object in Parse asynchronously)
+        pet.saveInBackground(block: success)
+        
+    }
+    
+    class func getPhotoFile(photo: UIImage?) -> PFFile? {
+        if let photo = photo {
+            if let photo_data = UIImagePNGRepresentation(photo) {
+                return PFFile(name: "photo.png", data: photo_data)
+            }
+        } else {
+            return nil
+        }
+        return nil
+    }
 }
