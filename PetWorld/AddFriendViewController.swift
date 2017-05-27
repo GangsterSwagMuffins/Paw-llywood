0eba9f2ea28b5a92536c2cebc37c0131cc9403d6
Vinnie Chen <vinnie.chen211@gmail.com>
2017-05-27 00:18:03 -0700

    Added friends tab

---
+++ PetWorld/AddFriendViewController.swift
@@ -0,0 +1,45 @@
+//
+//  AddFriendViewController.swift
+//  PetWorld
+//
+//  Created by Vinnie Chen on 5/27/17.
+//  Copyright © 2017 GangsterSwagMuffins. All rights reserved.
+//
+
+import UIKit
+
+class AddFriendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Add Friend will appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("Add Friend will disappear")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PetWorld/MyFriendsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MyFriendsViewController.swift
+//  PetWorld
+//
+//  Created by Vinnie Chen on 5/27/17.
+//  Copyright © 2017 GangsterSwagMuffins. All rights reserved.
+//
+
+import UIKit
+
+class MyFriendsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("My Friends VC will appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("My Friends will disappear")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
