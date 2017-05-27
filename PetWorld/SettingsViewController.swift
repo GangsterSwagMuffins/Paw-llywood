3362af3f49e93cba528f71b4add96dc3d772e0a9
Vivian Pham <Vivian@Vivians-MacBook-Pro.local>
2017-05-27 01:10:34 -0700

    Edited Profile and Setting Controllers

--- PetWorld/SettingsViewController.swift
+++ PetWorld/SettingsViewController.swift
@@ -8,14 +8,51 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var user: User!
+    var pet: Pet!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if(user == nil) {
+            if(User.current() == nil) {
+                tableView.reloadData();
+                return; // logged out
+            }
+            user = User.current();
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return user.petsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PetsTableViewCell", for: indexPath) as! PetsTableViewCell
+        
+        let pet = user.petsArray[indexPath.row]
+    /*
+        cell.nameLabel = pet.name
+        if (pet.image == nil) {
+            pet.image = #imageLiteral(resourceName: "Default Profile Image")
+        }
+        cell.imageView = pet.image
+ `*/
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
