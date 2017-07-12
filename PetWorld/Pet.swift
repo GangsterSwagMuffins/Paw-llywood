196d92405916be6d409da98da7b5c3845dbafc1f
EddieCurio <edcarril@ucsd.edu>
2017-07-11 19:42:12 -0700

    Added more fields to Pet class.

--- PetWorld/Pet.swift
+++ PetWorld/Pet.swift
@@ -11,13 +11,31 @@ import Parse
 
 class Pet: NSObject {
 
+    //Profile pictre
     var image: UIImage?
+    //Background profile picture
+    var backgroundImage: UIImage?
+    //Name of the pet.
     var name: String?
     var breed: String?
+    var species: String?
     var age: Int?
+    //Animals's favorite hobby
+    var hobby: String?
+    //Animal's favorite toy
+    var toy: String?
+    //Male or female none of this 55 gender stuff.
     var gender: String?
+    //Don't be a folllower be a leader.
     var followers: Int?
+    //Increase this.
     var following: Int?
+    //Mini bio (32 characters max)
+    var miniBio: String?
+    //Long bio (256 characters max)
+    var longBio: String?
+    
+    
     
     
     
@@ -27,7 +45,7 @@ class Pet: NSObject {
         pet["owner"] = PFUser.current() // Pointer column type that points to PFUser
         
         pet["name"] = name
-        pet["profile_picture"] =
+        //pet["profile_picture"] =
         
         // Save object (following function will save the object in Parse asynchronously)
         pet.saveInBackground(block: success)
