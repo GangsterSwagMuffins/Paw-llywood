7b26a67f13c8439f57b61d76da968a5c55e34fc7
EddieCurio <edcarril@ucsd.edu>
2017-07-18 18:47:13 -0700

    Creating a network layer.

---
+++ PetWorld/NetworkAPI.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkAPI.swift
+//  PetWorld
+//
+//  Created by my mac on 7/18/17.
+//  Copyright © 2017 GangsterSwagMuffins. All rights reserved.
+//
+
+import UIKit
+
+class NetworkAPI: NSObject {
+    
+    
+    
+
+}
